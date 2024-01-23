77bf49bdd24fba1be227b4e7db93cbcbe60857ef
Qian Qian <qiqian82@gmail.com>
2024-01-23 14:45:26 +0800

    gen header

---
+++ version.h
@@ -0,0 +1,17 @@
+// ===========================================================================
+// version.h
+// - collection of version numbers
+// ===========================================================================
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VER_MAJOR  1                // major version number
+#define VER_MINOR  2                // minor version number
+#define VER_BUILD  86             // build number
+
+#define VERSION_TYPE "release"
+#define VERSION_STR  "1.2.86"
+#define VERSION_SHA1 "1fd04917cff50fac72ae23e45f82ca6fd9130bd8"
+
+#endif // __VERSION_H__
